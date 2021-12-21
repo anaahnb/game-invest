d9379ecbb5993b7de8d83b574276121ff6b3f37d
Kelvyn Reis <kelvyn2reis@gmail.com>
2021-12-21 20:10:41 -0300

    create navegate saiba-mais js

---
+++ main.js
@@ -0,0 +1,11 @@
+//Codigo javascript pra navegação
+const button = document.getElementById('saber')
+
+button.addEventListener('click', () =>{
+    
+    location.href = "saiba-mais.html"
+}) 
+    
+
+
+
